932b9b1b8c452481c37f8b9ec77f2db17019c645
William Castañeda <william.castaneda@carrierssm.com>
2022-06-10 17:42:08 -0500

    linq to json & xml

--- linq-sample/Empleado.cs
+++ linq-sample/Empleado.cs
@@ -17,7 +17,7 @@ public class Empleado
 
 	}
 
-	public Guid Id { get; } = Guid.NewGuid();
+	public Guid Id { get; set; }
 	public string Nombre { get; set; }
 	public string Apellido { get; set; }
 	public Departamento Departamento { get; set; }
@@ -31,6 +31,7 @@ public class Pago
     public int PagoId { get; set; }
     public string? Descripcion { get; set; }
     public DateTime Fecha { get; set; }
+    public decimal Monto { get; set; }
     public bool Procesado { get; set; }
-    public int IdExternoEmpleado { get; set; }
+    public Guid IdExternoEmpleado { get; set; }
 }
--- linq-sample/Program.cs
+++ linq-sample/Program.cs
@@ -1,4 +1,8 @@
-﻿#region ejemplo de tulpa
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+#region ejemplo de tulpa
 //var usuario = (nombre: "William", apellido: "Castañeda");
 //Console.WriteLine($"Nombre: {usuario.nombre} {usuario.apellido}");
 #endregion 
@@ -31,20 +35,77 @@ List<Empleado> empleados = new List<Empleado>()
 {
     new Empleado
     {
+        Id = Guid.Parse("d65031f0-fdc7-44ae-8fff-56b066dc6e94"),
         Nombre = "William",
         Apellido = "Castañeda",
         Departamento = Departamento.Desarrollo,
-        Edad = 37
+        Edad = 37,
+        Pagos = new List<Pago>
+        {
+            new Pago
+            {
+                Descripcion = "Quincena 09",
+                Fecha = new DateTime(2022,05,01),
+                Monto = 20000,
+                Procesado = true,
+                IdExternoEmpleado = Guid.Parse("d65031f0-fdc7-44ae-8fff-56b066dc6e94")
+            },
+            new Pago
+            {
+                Descripcion = "Quincena 10",
+                Fecha = new DateTime(2022,05,31),
+                Monto = 21000,
+                Procesado = false,
+                IdExternoEmpleado = Guid.Parse("d65031f0-fdc7-44ae-8fff-56b066dc6e94")
+            }
+        }
     },
     new Empleado
     {
+        Id = Guid.Parse("120a56e8-994a-4fa7-b093-8cea9056c0af"),
         Nombre = "Carlos",
         Apellido = "Villarreal",
         Departamento = Departamento.Desarrollo,
-        Edad = 35
+        Edad = 35,
+        Pagos = new List<Pago>
+        {
+            new Pago
+            {
+                Descripcion = "Quincena 07",
+                Fecha = new DateTime(2022,04,01),
+                Monto = 22000,
+                Procesado = true,
+                IdExternoEmpleado = Guid.Parse("120a56e8-994a-4fa7-b093-8cea9056c0af")
+            },
+            new Pago
+            {
+                Descripcion = "Quincena 08",
+                Fecha = new DateTime(2022,04,30),
+                Monto = 23000,
+                Procesado = false,
+                IdExternoEmpleado = Guid.Parse("120a56e8-994a-4fa7-b093-8cea9056c0af")
+            },
+            new Pago
+            {
+                Descripcion = "Quincena 09",
+                Fecha = new DateTime(2022,05,01),
+                Monto = 22000,
+                Procesado = true,
+                IdExternoEmpleado = Guid.Parse("120a56e8-994a-4fa7-b093-8cea9056c0af")
+            },
+            new Pago
+            {
+                Descripcion = "Quincena 10",
+                Fecha = new DateTime(2022,05,31),
+                Monto = 23000,
+                Procesado = false,
+                IdExternoEmpleado = Guid.Parse("120a56e8-994a-4fa7-b093-8cea9056c0af")
+            }
+        }
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre = "David",
         Apellido = "Muñiz",
         Departamento = Departamento.Desarrollo,
@@ -52,20 +113,53 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.Parse("4c501b10-b26c-4f5b-a2be-3ea5d77c1ae4"),
         Nombre = "Joem",
         Apellido = "Sanchez",
         Departamento= Departamento.Soporte,
-        Edad = 35
+        Edad = 35,
+        Pagos = new List<Pago>
+        {
+            new Pago
+            {
+                Descripcion = "Quincena 10",
+                Fecha = new DateTime(2022,05,31),
+                Monto = 17000,
+                Procesado = true,
+                IdExternoEmpleado = Guid.Parse("4c501b10-b26c-4f5b-a2be-3ea5d77c1ae4")
+            }
+        }
     },
     new Empleado
     {
+        Id = Guid.Parse("0370e550-619f-4dcf-a50c-231b14da37f7"),
         Nombre = "Juan",
         Apellido= "Tirado",
         Departamento = Departamento.Soporte,
-        Edad = 42
+        Edad = 42,
+        Pagos = new List<Pago>
+        {
+            new Pago
+            {
+                Descripcion = "Quincena 09",
+                Fecha = new DateTime(2022,05,01),
+                Monto = 18000,
+                Procesado = true,
+                IdExternoEmpleado = Guid.Parse("0370e550-619f-4dcf-a50c-231b14da37f7")
+            },
+            new Pago
+            {
+                Descripcion = "Quincena 10",
+                Fecha = new DateTime(2022,05,31),
+                Monto = 19000,
+                Procesado = false,
+                IdExternoEmpleado = Guid.Parse("0370e550-619f-4dcf-a50c-231b14da37f7")
+            }
+        }
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre="Gerardo",
         Apellido="Garza",
         Departamento = Departamento.Soporte,
@@ -73,6 +167,7 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre="Adriana",
         Apellido="Martinez",
         Departamento=Departamento.Admin,
@@ -80,6 +175,7 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre="Lola",
         Apellido="Rincon",
         Departamento=Departamento.Admin,
@@ -87,6 +183,7 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre="Octavio",
         Apellido ="Muñiz",
         Departamento = Departamento.Admin,
@@ -94,6 +191,7 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre = "Angelica",
         Apellido = "Treviño",
         Departamento = Departamento.RH,
@@ -101,6 +199,7 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre = "Paloma",
         Apellido = "Picos",
         Departamento = Departamento.RH,
@@ -108,38 +207,13 @@ List<Empleado> empleados = new List<Empleado>()
     },
     new Empleado
     {
+        Id = Guid.NewGuid(),
         Nombre = "Estefania",
         Apellido = "Martinez",
         Departamento = Departamento.RH,
         Edad = 41
     }
 };
-
-#region test1
-//var devs = empleados.Where(x => x.Departamento == Departamento.Desarrollo && x.Apellido.ToUpper().StartsWith("W")).OrderBy(y => y.Id);
-//foreach (var dev in devs)
-//{
-//    Console.WriteLine($"Nombre: {dev.Nombre} {dev.Apellido}, Departamento: {dev.Departamento}");
-//}
-
-//var emp = empleados.Where(x => x.Apellido.ToUpper().StartsWith("M")).OrderBy(y => y.Id).Select(u => new
-//{
-//    u.Id,
-//    u.Nombre,
-//    InicialAp = u.Apellido.Substring(0,1),
-//    u.Apellido,
-//    u.Departamento
-//});
-
-//var encabezado = string.Format("{0,-40}, {1,-10}, {2,-10}, {3,-10}, {4}", "ID", "Nombre", "InicialAp", "Apellido", "Departamento");
-//Console.WriteLine(encabezado);
-
-//foreach (var em in emp)
-//{
-//    var fila = string.Format("{0,-40}, {1,-10}, {2,-10}, {3,-10}, {4}", em.Id, em.Nombre, em.InicialAp, em.Apellido, em.Departamento);
-//    Console.WriteLine(fila);
-//}
-#endregion
 
 #region test2
 //var emps = empleados
@@ -212,6 +286,159 @@ List<Empleado> empleados = new List<Empleado>()
 
 #endregion
 
+#region test5
+
+////obtener todos los pagos de los empleados del departamento de desarrollo regresa una secuencia de secuencias
+//var pagos = empleados.Where(x => x.Departamento == Departamento.Desarrollo).Select(e => e.Pagos);
+
+////obtiene todos los pagos de los empleados del departamento de desarrollo regresa un enumerable de pagos
+//var pagos2 = empleados.Where(x => x.Departamento == Departamento.Desarrollo && x.Pagos != null).SelectMany(e => e.Pagos);
+
+//var cantidadPagos = pagos2.Count();
+//var promedioPagos = pagos2.Average(p => p.Monto);
+//var pagoMin = pagos2.Min(p => p.Monto);
+
+//decimal monto = 20000;
+////regresa true si existe algun pago menor que 20,000
+//var existePagoMenor = pagos2.Any(p => p.Monto <= monto);
+////regresa true si todos los pagos son menores a 20,000
+//var todosPagosMenor = pagos2.All(p => p.Monto <= monto);
+
+//var pago = pagos2.FirstOrDefault(p => p.Monto == 22000);
+//Console.WriteLine($"Pago {pago.Monto}");
+
+//var spago = pagos2.SingleOrDefault(p => p.Monto == 20000); //22,000 marca error
+//Console.WriteLine($"Pago {spago.Monto}");
+
+//var elpago = pagos2.ElementAt(2);
+//Console.WriteLine($"Pago {elpago.Monto}");
+
+#endregion
+
+#region test6
+
+//var nPagos = new List<Pago>
+//{
+//    new Pago
+//    {
+//        Descripcion = "Quincena 07",
+//        Fecha = new DateTime(2022,04,01),
+//        Monto = 18000,
+//        Procesado = true,
+//        IdExternoEmpleado = Guid.Parse("d65031f0-fdc7-44ae-8fff-56b066dc6e94")
+//    },
+//    new Pago
+//    {
+//        Descripcion = "Quincena 08",
+//        Fecha = new DateTime(2022,04,30),
+//        Monto = 19000,
+//        Procesado = false,
+//        IdExternoEmpleado = Guid.Parse("d65031f0-fdc7-44ae-8fff-56b066dc6e94")
+//    },
+//    new Pago
+//    {
+//        Descripcion = "Quincena 09",
+//        Fecha = new DateTime(2022,05,01),
+//        Monto = 19000,
+//        Procesado = false,
+//        IdExternoEmpleado = Guid.Parse("4c501b10-b26c-4f5b-a2be-3ea5d77c1ae4")
+//    }
+//};
+
+////join
+//var empleadosPagosJoin = empleados.Join(nPagos,
+//                                        emp => emp.Id,
+//                                        pago => pago.IdExternoEmpleado,
+//                                        (emp, pago) => new
+//                                        {
+//                                            emp.Nombre,
+//                                            pago.Monto
+//                                        });
+
+
+//var empleadosPagosGroupJoin = empleados.GroupJoin(nPagos,
+//                                        emp => emp.Id,
+//                                        pago => pago.IdExternoEmpleado,
+//                                        (emp, pago) => new
+//                                        {
+//                                            emp.Nombre,
+//                                            pagoagregados = pago
+//                                        });
+
+//foreach (var e in empleadosPagosGroupJoin)
+//{
+//    if (e.pagoagregados.Count() > 0)
+//        Console.WriteLine(e.Nombre);
+//    foreach (var p in e.pagoagregados)
+//        Console.WriteLine(p.Monto);
+//}
+
+#endregion
+
+#region json to collection
+
+//var archivo = Path.Combine(AppContext.BaseDirectory,"empleados.json");
+//var import = ObtenerEmpleados(archivo);
+//var nombre = empleados.Where(e => e.Edad <= 35)
+//    .Select(e => e.Nombre).FirstOrDefault();
+//Console.WriteLine(nombre);
+
+#endregion
+
+#region json object
+
+//JObject obj = JObject.Parse(@"{
+//              'nombre': 'William Castañeda',
+//              'nivel': 'Sr',
+//              'edad': 37,
+//              'lenguajes': [ '.NET', 'Javascript', 'Pyton' ]
+//            }");
+//string nivel = (string)obj["nivel"];
+//Console.WriteLine(nivel);
+
+//IList<string> lenguajes = obj["lenguajes"].Select(l => (string)l).ToList();
+//foreach (var l in lenguajes)
+//{
+//    Console.WriteLine(l);
+//}
+
+#endregion
+
+#region read xml linq
+
+//var archivo = Path.Combine(AppContext.BaseDirectory, "pagos.xml");
+//var docXml = XDocument.Load(archivo);
+//var pagosProcesados = docXml.Element("pagos").Elements("pago")
+//    .Where(p => p.Attribute("procesado")?.Value == "true")
+//    //.Where(p => p.Attribute("firmado")?.Value == "true")
+//    .Select(p => p.Element("descripcion").Value);
+
+//foreach (var p in pagosProcesados)
+//{
+//    Console.WriteLine(p);
+//}
+
+#endregion
+
+#region xml to object (tupla)
+
+var archivo = @"C:\Users\noemi\source\repos\ConsoleApp1\pagos.xml";
+var docXml = XDocument.Load(archivo);
+
+var pagos = docXml.Descendants("pago").
+    Where(p => p.Attribute("procesado")?.Value == "true")
+    .Select(p => new Tuple<string, bool, string, float>
+        (
+            p.Attribute("idEmpleado").Value,
+            bool.Parse(p.Attribute("firmado").Value),
+            p.Element("descripcion").Value,
+            float.Parse(p.Element("montoBase").Value)
+        ));
+
+#endregion
+
+Console.ReadLine();
+
 #region metodos imprimir
 static void ImprimirEmpleado(Empleado e)
 {
@@ -229,5 +456,18 @@ static void ImprimirEmpleados(IEnumerable<Empleado> lista, string titulo = "/** 
     {
         ImprimirEmpleado(emp);
     }
+}
+#endregion
+
+#region metodos import
+static List<Empleado> ObtenerEmpleados(string ruta)
+{
+    List<Empleado> listaE = new List<Empleado>();
+    using (System.IO.StreamReader r = new System.IO.StreamReader(ruta))
+    {
+        string json = r.ReadToEnd();
+        listaE = JsonConvert.DeserializeObject<List<Empleado>>(json);
+    }
+    return listaE;
 }
 #endregion
